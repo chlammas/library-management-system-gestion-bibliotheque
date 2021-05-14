73f338c74d378d3443494883df0c14c4e4939f27
chouaib lammas <chouaib036@gmail.com>
2021-05-14 20:00:52 +0000

    Count number of out of stock books

--- app/helpers/statistics_helper.php
+++ app/helpers/statistics_helper.php
@@ -3,6 +3,7 @@ class Statistics extends Controller
 {
   static Borrowing $borrowingModel;
   static Reservation $reservationModel;
+  static Book $bookModel;
 
   static function reservationsCount()
   {
@@ -16,5 +17,11 @@ class Statistics extends Controller
     require_once '../app/models/Borrowing.php';
     self::$borrowingModel = new Borrowing;
     return count(self::$borrowingModel->getBorrowings(false));
+  }
+  static function OutOfStockBooksCount()
+  {
+    require_once '../app/models/Borrowing.php';
+    self::$bookModel = new Book;
+    return count(self::$bookModel->findBooks(false));
   }
 }
