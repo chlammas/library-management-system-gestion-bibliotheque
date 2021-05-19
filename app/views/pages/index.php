bc3b592849f2030ac90c2de41fe9cefd3bd22be9
chouaib lammas <chouaib036@gmail.com>
2021-05-19 21:00:49 +0000

    Update nav links color

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -2,21 +2,21 @@
 <div class="carsd login-caard">
   <div class="row no-gutters">
     <div class="col-md-6">
-      <img src="<?php echo URLROOT ?>/img/library.jpg" alt="login" class="login-card-img">
+      <img src="<?php echo URLROOT ?>/img/<?=$data['type'] === 'admin' ? 'admin-login' : 'library' ?>.jpg" alt="login" class="login-card-img">
 
     </div>
     <div class="col-md-6">
 
       <div class="card-body card-body-login card-borrower">
-        <p class="login-card-description">Use your library card's code to sign into your account</p>
+        <p class="login-card-description"><?=$language['login_card_description']?></p>
 
         <form action="<?php echo URLROOT . ($data['type'] === 'admin' ? '/admins/login' : '/borrowers/login') ?>" method="POST" class="row g-3">
           <div class="form-group">
-            <input type="text" required name="barcode" placeholder="Enter your card code..." class="form-control form-control-lg <?php echo (!empty($data['barcode_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['barcode']; ?>">
+            <input type="text" required name="barcode" placeholder="<?=$language['login_placeholder']?>" class="form-control form-control-lg <?php echo (!empty($data['barcode_err'])) ? 'is-invalid' : ''; ?>" value="<?php echo $data['barcode']; ?>">
             <span class="invalid-feedback"><?php echo $data['barcode_err']; ?></span>
           </div>
           <div class="form-group">
-            <button type="submit" class="btn btn-outline-primary  mb-3">Login</button>
+            <button type="submit" class="btn btn-outline-primary  mb-3"><?=$language['btn_login']?></button>
           </div>
         </form>
 
@@ -31,15 +31,14 @@
         <div class="modal-dialog">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="staticBackdropLabel">Login As</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              <h5 class="modal-title" id="staticBackdropLabel"><?=$language['login_as']?></h5>
             </div>
             <div class="modal-body">
-              Please choose which type of accounts would you test ?
+            <?=$language['login_as_msg']?>
             </div>
             <div class="modal-footer">
-              <a href="<?= URLROOT . '/borrower' ?>" class="btn btn-outline-primary">Borrower <i class="fas fa-user"></i></a>
-              <a href="<?= URLROOT . '/admin' ?>" class="btn btn-outline-primary">Admin <i class="fas fa-user-cog"></i></a>
+              <a href="<?= URLROOT . '/borrower' ?>" class="btn btn-outline-primary"><?=$language['login_as_btn_borrower']?> <i class="fas fa-user"></i></a>
+              <a href="<?= URLROOT . '/admin' ?>" class="btn btn-outline-primary"><?=$language['login_as_btn_admin']?> <i class="fas fa-user-cog"></i></a>
             </div>
           </div>
         </div>
