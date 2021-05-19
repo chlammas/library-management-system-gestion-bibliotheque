8b41372cfd455db605b16f6270a578b338606fe5
chouaib lammas <chouaib036@gmail.com>
2021-05-19 09:17:05 +0000

    update config file

--- app/config/config.php
+++ app/config/config.php
@@ -1,17 +1,17 @@
 <?php
 // Db params
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'library');
+define('DB_USER', '_your_db_username_'); // ex: root
+define('DB_PASS', '_your_db_pass_'); // usualy empty if you use default root user in xampp
+define('DB_NAME', '_your_db_name_'); // ex : library_db
 
 
 
 //App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 //URL Root
-define('URLROOT', 'http://localhost/gestion_bibliotheque');
+define('URLROOT', '_url_root_'); // ex: http://localhost/library
 //Site Name
-define('SITENAME', 'Gestion de bibliothèque');
+define('SITENAME', 'Library Management System');
 
 
