1238a367a11cd5d84dd91825d886644fd78b3230
chouaib lammas <chouaib036@gmail.com>
2021-05-21 12:43:03 +0000

    Update borrower side translation

--- app/views/inc/navbar.php
+++ app/views/inc/navbar.php
@@ -27,7 +27,7 @@
           <li>
             <hr class="dropdown-divider">
           </li>-->
-          <li><a class="dropdown-item" href="<?php echo URLROOT ?>/borrowers/logout">Logout</a></li>
+          <li><a class="dropdown-item" href="<?php echo URLROOT ?>/borrowers/logout"><?= $language['btn_logout'] ?></a></li>
         </ul>
       </div>
     <?php elseif (isAdminLoggedIn()) : ?>
